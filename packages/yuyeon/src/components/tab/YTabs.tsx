c3d5a66134a016e2cecf2259e2529e775a45483d
yeonyew <hayan@yeonyu.dev>
2023-09-07 10:31:33 +0900

    feat: tabs copy code vuetify

---
+++ packages/yuyeon/src/components/tab/YTabs.tsx
@@ -0,0 +1,40 @@
+import { PropType, defineComponent, ref } from 'vue';
+
+import { useRender } from '../../composables/component';
+import { useResizeObserver } from '../../composables/resize-observer';
+import { propsFactory } from '../../util/vue-component';
+
+export const pressYTabsPropOptions = propsFactory({}, 'YTabs');
+
+export const YTabs = defineComponent({
+  name: 'YTabs',
+  props: {
+    tag: {
+      type: String as PropType<'div' | 'nav' | 'ol' | 'ul'>,
+      default: 'div',
+    },
+  },
+  setup(props, { slots }) {
+    const { resizeObservedRef: container$, contentRect: containerRect } =
+      useResizeObserver();
+    const { resizeObservedRef: content$, contentRect } = useResizeObserver();
+
+    useRender(() => {
+      return (
+        <props.tag class={['y-tabs']}>
+          <div
+            key="container"
+            ref={container$}
+            class={['y-tabs__container']}
+          >
+            <div key="content" ref={content$}>
+              {slots.default?.()}
+            </div>
+          </div>
+        </props.tag>
+      );
+    });
+
+    return {};
+  },
+});
