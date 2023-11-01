dd04d05f54f85078fa53963f060e93606d13bc9f
연유 <yeonyew@gmail.com>
2023-11-01 10:14:48 +0900

    fix: multiple select option fix: selected item active class

--- packages/yuyeon/src/components/alert/YAlert.tsx
+++ packages/yuyeon/src/components/alert/YAlert.tsx
@@ -2,28 +2,57 @@ import { PropType, defineComponent, ref } from 'vue';
 
 import { useRender } from '../../composables/component';
 import { toKebabCase } from '../../util/string';
+import { YPlate } from '../plate';
+
+import './YAlert.scss';
 
 const NAME = 'YAlert';
 const KEBAB_NAME = toKebabCase(NAME);
 
 const YAlertPropOptions = {
-    outlined: {
-        type: Boolean as PropType<boolean>,
-    },
+  semantic: String as PropType<
+    'info' | 'warning' | 'success' | 'error' | string
+  >,
+  variation: String as PropType<'outlined' | 'filled' | string>,
+  color: String as PropType<string>,
+  textColor: String as PropType<string>,
+  outlineColor: String as PropType<string>,
 };
 
 /**
  * #  Component
  */
 export const YAlert = defineComponent({
-    name: NAME,
-    props: {
-        ...YAlertPropOptions,
-    },
-    setup(looseProps, { slots }) {
-        const props = defineProps(YAlertPropOptions);
-        const el$ = ref<HTMLElement>();
+  name: NAME,
+  props: {
+    ...YAlertPropOptions,
+  },
+  setup(looseProps, { slots }) {
+    const el$ = ref<HTMLElement>();
 
-        useRender(() => <div ref={el$}>{slots.default?.()}</div>);
-    },
+    useRender(() => (
+      <div
+        ref={el$}
+        class={[
+          KEBAB_NAME,
+          { [`y-alert--${looseProps.semantic}`]: looseProps.semantic },
+        ]}
+        v-theme
+      >
+        <YPlate></YPlate>
+        {slots.leading && (
+          <div class={['y-alert__leading']}>{slots.leading()}</div>
+        )}
+        <div class={['y-alert__content']}>
+          {slots.title && <div class={['y-alert__title']}>{slots.title()}</div>}
+          {slots.default?.()}
+        </div>
+        {slots.trailing && (
+          <div class={['y-alert__trailing']}>{slots.trailing()}</div>
+        )}
+      </div>
+    ));
+  },
 });
+
+export type YAlert = InstanceType<typeof YAlert>;
