129fddd2c8003d12a0298cbcb35aab737d1887a1
연유 <yeonyew@gmail.com>
2024-09-06 11:01:41 +0900

    fix: [icon] Resolve type conflict in JSXComponent's Props.

--- packages/yuyeon/src/types/index.ts
+++ packages/yuyeon/src/types/index.ts
@@ -1,7 +1,7 @@
-import { HTMLAttributes } from '@vue/runtime-dom';
 import type { ComponentPublicInstance, FunctionalComponent } from 'vue';
 
 export type CandidateKey = string | number;
+
 export type JSXComponent<Props = any> =
-  | FunctionalComponent<Props | Props & HTMLAttributes>
-  | { new (): ComponentPublicInstance<Props | Props & HTMLAttributes> };
+  | FunctionalComponent<Props>
+  | { new (): ComponentPublicInstance<Props> };
