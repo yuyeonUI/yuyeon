75a6b744e739e9f66a9bdde6dae349c750bbfc56
연유 <yeonyew@gmail.com>
2025-04-22 17:15:10 +0900

    fix: header no longer uses Array.at method
    - update babel, core-js
    - export lib file extension .mjs to .js

--- packages/yuyeon/babel.config.cjs
+++ packages/yuyeon/babel.config.cjs
@@ -1,36 +1,48 @@
-const packageSpec = require('./package.json')
+const packageSpec = require('./package.json');
 
 module.exports = {
   assumptions: {
-    noDocumentAll: true
+    noDocumentAll: true,
   },
   ignore: [/\.d\.ts$/],
   presets: [
-    ['@babel/preset-env', {
-      modules: false,
-    }],
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+      },
+    ],
     '@babel/preset-typescript',
   ],
   plugins: [
     ['@vue/babel-plugin-jsx', { optimize: false, enableObjectSlots: false }],
-    ['transform-define', {
-      __YUYEON_VERSION__: packageSpec.version,
-      __REQUIRED_VUE__: packageSpec.peerDependencies.vue,
-    }],
-    ['module-resolver', {
-      root: ['.'],
-      alias: {
-        '@': './src',
+    [
+      'transform-define',
+      {
+        __YUYEON_VERSION__: packageSpec.version,
+        __REQUIRED_VUE__: packageSpec.peerDependencies.vue,
       },
-    }],
+    ],
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
   ],
   env: {
     test: {
       presets: [
-        ['@babel/preset-env', {
-          targets: { node: true },
-          modules: 'commonjs',
-        }],
+        [
+          '@babel/preset-env',
+          {
+            targets: { node: true },
+            modules: 'commonjs',
+          },
+        ],
       ],
     },
     lib: {
@@ -44,4 +56,4 @@ module.exports = {
       ],
     },
   },
-}
+};
