9f1cf76e24b82feb4ec538b28b99e9ccc93fe58b
연유 <yeonyew@gmail.com>
2026-03-04 16:54:12 +0900

    fix: [YDatePicker] use `month` and `year` props to control calendar display

--- packages/yuyeon/src/components/date-picker/YDatePicker.tsx
+++ packages/yuyeon/src/components/date-picker/YDatePicker.tsx
@@ -1,160 +1,185 @@
-import { computed, ref, Transition, watch } from "vue";
+import { computed, ref, Transition, watch } from 'vue';
 
-import { useModelDuplex } from "@/composables/communication";
-import { useRender } from "@/composables/component";
-import { useDate } from "@/composables/date";
+import { useModelDuplex } from '@/composables/communication';
+import { useRender } from '@/composables/component';
+import { useDate } from '@/composables/date';
 import { omit } from '@/util';
-import { chooseProps, defineComponent, propsFactory } from "@/util/component";
+import { chooseProps, defineComponent, propsFactory } from '@/util/component';
 
-import { pressYDateCalendarPropsOptions, YDateCalendar } from "./YDateCalendar";
-import { YDatePickerControl } from "./YDatePickerControl";
-import { YMonthPicker } from "./YMonthPicker";
-import { YYearPicker } from "./YYearPicker";
+import { pressYDateCalendarPropsOptions, YDateCalendar } from './YDateCalendar';
+import { YDatePickerControl } from './YDatePickerControl';
+import { YMonthPicker } from './YMonthPicker';
+import { YYearPicker } from './YYearPicker';
 
-import "./YDatePicker.scss";
+import './YDatePicker.scss';
 
 export const pressYDatePickerPropsOptions = propsFactory(
-	{
-		...omit(pressYDateCalendarPropsOptions(), ["modelValue"]),
-		modelValue: null,
-	},
-	"YDatePicker",
+  {
+    ...omit(pressYDateCalendarPropsOptions(), ['modelValue']),
+    modelValue: null,
+  },
+  'YDatePicker',
 );
 
 export const YDatePicker = defineComponent({
-	name: "YDatePicker",
-	props: pressYDatePickerPropsOptions(),
-	emits: ["update:month", "update:year", "update:modelValue", "update:mode", "click:day"],
-	setup(props, { emit }) {
-		const yearPicker$ = ref<typeof YYearPicker>();
-		const dateUtil = useDate();
-		const model = useModelDuplex(props, "modelValue");
-		const mode = useModelDuplex(props, "mode");
+  name: 'YDatePicker',
+  props: pressYDatePickerPropsOptions(),
+  emits: [
+    'update:month',
+    'update:year',
+    'update:modelValue',
+    'update:mode',
+    'click:day',
+  ],
+  setup(props, { emit }) {
+    const yearPicker$ = ref<typeof YYearPicker>();
+    const dateUtil = useDate();
+    const model = useModelDuplex(props, 'modelValue');
+    const mode = useModelDuplex(props, 'mode');
 
-		const month = ref<number>(dateUtil.getMonth(dateUtil.date()));
-		const year = ref<number>(dateUtil.getYear(dateUtil.date()));
+    const month = useModelDuplex(props, 'month');
+    const year = useModelDuplex(props, 'year');
 
-		const propMonth = Number(props.month);
-		if (!isNaN(propMonth)) month.value = propMonth;
-		const propYear = Number(props.year);
-		if (!isNaN(propYear)) year.value = propYear;
+    const controlMonth = computed({
+      get: () => {
+        return month.value != null || !Number.isNaN(Number(month.value))
+          ? Number(month.value)
+          : dateUtil.getMonth(dateUtil.date());
+      },
+      set: (value: number) => {
+        controlMonth.value = value;
+      },
+    });
 
-		const displayDate = computed(() => {
-			let date = dateUtil.startOfMonth(dateUtil.date());
-			date = dateUtil.setYear(date, +year.value);
-			return dateUtil.setMonth(date, +month.value);
-		});
+    const controlYear = computed({
+      get: () => {
+        return year.value != null || !Number.isNaN(Number(year.value))
+          ? Number(year.value)
+          : dateUtil.getYear(dateUtil.date());
+      },
+      set: (value: number) => {
+        controlYear.value = value;
+      },
+    });
 
-		const monthText = computed(() => {
-			return dateUtil.format(displayDate.value, "month");
-		});
+    const displayDate = computed(() => {
+      let date = dateUtil.startOfMonth(dateUtil.date());
+      date = dateUtil.setYear(date, +controlYear.value);
+      return dateUtil.setMonth(date, +controlMonth.value);
+    });
 
-		const yearText = computed(() => {
-			return dateUtil.format(displayDate.value, "year");
-		});
+    const monthText = computed(() => {
+      return dateUtil.format(displayDate.value, 'month');
+    });
 
-		function toggleMonthMode() {
-			mode.value = mode.value === "month" ? "date" : "month";
-		}
+    const yearText = computed(() => {
+      return dateUtil.format(displayDate.value, 'year');
+    });
 
-		function toggleYearMode() {
-			mode.value = mode.value === "year" ? "date" : "year";
-		}
+    function toggleMonthMode() {
+      mode.value = mode.value === 'month' ? 'date' : 'month';
+    }
 
-		function onClickYear() {
-			toggleYearMode();
-		}
+    function toggleYearMode() {
+      mode.value = mode.value === 'year' ? 'date' : 'year';
+    }
 
-		function onClickMonth() {
-			toggleMonthMode();
-		}
+    function onClickYear() {
+      toggleYearMode();
+    }
 
-		function onClickPage(dir: number) {
-			if (mode.value === "month") {
-				year.value = year.value + dir;
-				return;
-			}
-			if (mode.value === "year") {
-				yearPicker$.value?.changePage(dir);
-				return;
-			}
-			const change = month.value + dir;
-			if (change > 11) {
-				year.value += 1;
-				month.value = 0;
-			} else if (change < 0) {
-				year.value -= 1;
-				month.value = 11;
-			} else {
-				month.value = change;
-			}
-		}
+    function onClickMonth() {
+      toggleMonthMode();
+    }
 
-		function onClickPrev() {
-			onClickPage(-1);
-		}
+    function onClickPage(dir: number) {
+      if (mode.value === 'month') {
+        controlYear.value = controlYear.value + dir;
+        return;
+      }
+      if (mode.value === 'year') {
+        yearPicker$.value?.changePage(dir);
+        return;
+      }
+      const change = controlMonth.value + dir;
+      if (change > 11) {
+        controlYear.value += 1;
+        controlMonth.value = 0;
+      } else if (change < 0) {
+        controlYear.value -= 1;
+        controlMonth.value = 11;
+      } else {
+        controlMonth.value = change;
+      }
+    }
 
-		function onClickNext() {
-			onClickPage(1);
-		}
+    function onClickPrev() {
+      onClickPage(-1);
+    }
+
+    function onClickNext() {
+      onClickPage(1);
+    }
 
     function onClickDay(item: any) {
       emit('click:day', item.date);
     }
 
-		watch(month, () => {
-			if (mode.value === "month") toggleMonthMode();
-			emit("update:month", month.value);
-		});
+    watch(controlMonth, () => {
+      if (mode.value === 'month') toggleMonthMode();
+      emit('update:month', controlMonth.value);
+    });
 
-		watch(year, () => {
-			if (mode.value === "year") {
-				mode.value = "month";
-			}
-			emit("update:year", year.value);
-		});
+    watch(controlYear, () => {
+      if (mode.value === 'year') {
+        mode.value = 'month';
+      }
+      emit('update:year', controlYear.value);
+    });
 
-		useRender(() => (
-			<div class={["y-date-picker"]}>
-				<YDatePickerControl
-					{...chooseProps(props, YDatePickerControl.props)}
-					yearText={yearText.value}
-					monthText={monthText.value}
-					onClick:year={onClickYear}
-					onClick:month={onClickMonth}
-					onClick:prev={onClickPrev}
-					onClick:next={onClickNext}
-				></YDatePickerControl>
-				<Transition name="fade" mode="out-in">
-					{mode.value === "month" ? (
-						<YMonthPicker
-							v-model={month.value}
-							onMode={() => {
-								mode.value = "date";
-							}}
-						/>
-					) : mode.value === "year" ? (
-						<YYearPicker
-							v-model={year.value}
-							ref={yearPicker$}
-							onMode={() => {
-								mode.value = "month";
-							}}
-						/>
-					) : (
-						<YDateCalendar
-							{...omit(chooseProps(props, YDateCalendar.props), ['onClick:day'])}
-							hideHeader={true}
-							v-model={model.value}
-							v-model:month={month.value}
-							v-model:year={year.value}
+    useRender(() => (
+      <div class={['y-date-picker']}>
+        <YDatePickerControl
+          {...chooseProps(props, YDatePickerControl.props)}
+          yearText={yearText.value}
+          monthText={monthText.value}
+          onClick:year={onClickYear}
+          onClick:month={onClickMonth}
+          onClick:prev={onClickPrev}
+          onClick:next={onClickNext}
+        ></YDatePickerControl>
+        <Transition name="fade" mode="out-in">
+          {mode.value === 'month' ? (
+            <YMonthPicker
+              v-model={controlMonth.value}
+              onMode={() => {
+                mode.value = 'date';
+              }}
+            />
+          ) : mode.value === 'year' ? (
+            <YYearPicker
+              v-model={controlYear.value}
+              ref={yearPicker$}
+              onMode={() => {
+                mode.value = 'month';
+              }}
+            />
+          ) : (
+            <YDateCalendar
+              {...omit(chooseProps(props, YDateCalendar.props), [
+                'onClick:day',
+              ])}
+              hideHeader={true}
+              v-model={model.value}
+              v-model:month={controlMonth.value}
+              v-model:year={controlYear.value}
               onClick:day={onClickDay}
-						/>
-					)}
-				</Transition>
-			</div>
-		));
-	},
+            />
+          )}
+        </Transition>
+      </div>
+    ));
+  },
 });
 
 export type YDatePicker = InstanceType<typeof YDatePicker>;
